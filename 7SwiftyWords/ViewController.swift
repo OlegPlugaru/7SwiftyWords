fa30ef0f402496b0133679ef90adf58ed1a603c6
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-10-01 21:28:09 +0300

    Initial Commit

---
+++ 7SwiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7SwiftyWords
+//
+//  Created by Oleg Plugaru on 01.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
